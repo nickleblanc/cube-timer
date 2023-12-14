8b9cd562e85b6011103a4e9d98d060fcfacbab51
Nick <nick.leblanc@unb.ca>
2023-12-13 22:35:58 -0400

    added basic timer

--- app/components/timer-display.tsx
+++ app/components/timer-display.tsx
@@ -5,17 +5,36 @@ import { useState, useEffect } from 'react';
 export default function TimerDisplay() {
 
     const [time, setTime] = useState(0);
+    const [running, setRunning] = useState(false);
+    const [startTime, setStartTime] = useState(0);
+
+    const startTimer = () => {
+        setRunning(true);
+        setStartTime(Date.now());
+    }
+
+    const formattedTime = () => {
+        const minutes = Math.floor(time / 60000);
+        const seconds = Math.floor((time % 60000) / 1000);
+        const milliseconds = Math.floor(time % 1000);
+        return `${minutes}:${seconds}:${milliseconds}`;
+      };
 
     useEffect(() => {
+        if (!running) {
+            return;
+        }
         const interval = setInterval(() => {
-            setTime(time => time + 1);
-        }, 1000);
+            setTime(Date.now() - startTime);
+        }, 10);
         return () => clearInterval(interval);
-    }, []);
+    }, [time, startTime, running]);
 
     return (
         <div className="flex justify-center items-center h-64">
-            {time}
+            {/* {time} */}
+            {formattedTime()}
+            <button onClick={startTimer}>Start</button>
         </div>
     )
 }
