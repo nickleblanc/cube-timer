77f089d7d41daadf8b2894f8573bddf5bd9072fc
Nick Leblanc <nick.leblanc@unb.ca>
2024-01-17 21:33:14 -0400

    refactored to use timestring utility

--- app/solves/page.tsx
+++ app/solves/page.tsx
@@ -1,6 +1,7 @@
 import NavBar from "@/app/components/navbar"
 import Modal from "@/app/components/modal"
 import { fetchSolveData } from "@/app/lib/data"
+import getTimeString from "@/app/lib/timer-util";
 // import { useState } from "react"
 
 export default async function Solves() {
@@ -10,16 +11,10 @@ export default async function Solves() {
     // const [show, setShow] = useState(false);
 
     const solveList = solves.map((solve) => {
-        let time = solve.time;
-        let minutes = Math.floor(time / 6000);
-        let seconds = Math.floor((time / 1000) % 60);
-        let milliseconds = Math.floor(time % 1000);
-        let timeFormat = `${minutes}:${seconds
-          .toString()
-          .padStart(2, "0")}.${milliseconds.toString().padStart(3, "0")}`;
+        const time = getTimeString(solve.time);
         return (
           <div key={solve.id} className="bg-slate-600 rounded-xl w-96 h-24 p-4 m-1">
-            <span>{timeFormat}</span>
+            <span>{time}</span>
           </div>
         );
       }
