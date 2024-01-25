e9a932715e9c8e69a30c7f41e7ddb4ee81854349
Nick Leblanc <nick.leblanc@unb.ca>
2024-01-24 20:22:17 -0400

    Add register page component

---
+++ app/register/page.tsx
@@ -0,0 +1,9 @@
+import { RegisterForm } from "@/app/components/auth/register-form"
+
+export default function() {
+    return (
+        <div className="flex justify-center">
+            <RegisterForm />
+        </div>
+    )
+}
