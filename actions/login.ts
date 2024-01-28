7a0fa0013b7a350b5cfe35e08e3ebcc31c7a0d2c
Nick Leblanc <nick.leblanc@unb.ca>
2024-01-28 11:45:07 -0400

    Add login action

---
+++ actions/login.ts
@@ -0,0 +1,41 @@
+'use server';
+
+import { z } from 'zod';
+import { signIn } from '@/auth'
+import { getUserByEmail } from '@/data/user'
+import { LoginSchema } from '@/schemas';
+import { AuthError } from 'next-auth';
+
+export async function login(values: z.infer<typeof LoginSchema>) {
+    const validatedFields = LoginSchema.safeParse(values);
+
+    if (!validatedFields.success) {
+        return { error: "Invalid fields!" };
+    }
+
+    const { email, password } = validatedFields.data;
+
+    const existingUser = await getUserByEmail(email);
+
+    if (!existingUser || !existingUser.email || !existingUser.password) {
+        return { error: "Email does not exist!" };
+    }
+
+    try {
+        await signIn('credentials', {
+            email,
+            password,
+            redirectTo: '/timer'
+        })
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case 'CredentialsSignin':
+                    return { error: 'Invalid credentials!' }
+                default:
+                    return { error: 'Something went wrong!' }
+            }
+        }
+        throw error;
+    }
+}
