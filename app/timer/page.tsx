838c07db59292f9f0ffc739f82da5db36c02b1c5
Nick Leblanc <nick.leblanc@unb.ca>
2024-01-28 11:58:29 -0400

    Remove unused imports and commented code

--- app/timer/page.tsx
+++ app/timer/page.tsx
@@ -5,13 +5,9 @@ import NavBar from '@/app/components/navbar'
 import TimerDisplay from '@/app/components/timer-display'
 import Scramble from '@/app/components/scramble'
 import Stats from '@/app/components/stats'
-import { getSolves } from "@/app/lib/data";
-import { revalidatePath } from 'next/cache';
 import Timer from './timer';
 
 export default async function Home() {
-
-  // const data = await getSolves();
 
   return (
     <>
