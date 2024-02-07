7fb98e43282c6df441067c1d1ce3b824bd6ca281
Nick <nick.leblanc@unb.ca>
2024-02-07 13:48:25 -0400

    Added settings page

---
+++ app/settings/page.tsx
@@ -0,0 +1,14 @@
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+
+export default function Settings() {
+  return (
+    <div className="m-10 flex justify-center">
+      <Card className="m-1 h-[500px] w-[500px] text-lg">
+        <CardHeader>
+          <CardTitle className="text-center">Settings</CardTitle>
+        </CardHeader>
+        <CardContent></CardContent>
+      </Card>
+    </div>
+  );
+}
