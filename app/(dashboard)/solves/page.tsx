23b30298c9615dffea1c576d7930d7a62246dfc5
Nick <nick.leblanc@unb.ca>
2024-02-20 11:22:53 -0400

    Changed to only fetch users solves

--- app/(dashboard)/solves/page.tsx
+++ app/(dashboard)/solves/page.tsx
@@ -1,10 +1,16 @@
-import NavBar from "@/components/navbar";
-import { getAllSolves } from "@/data/solve";
+import { getSolvesByUser } from "@/data/solve";
 import { getTimeString } from "@/lib/timer-util";
 import { SolveCard } from "@/components/solve-card";
+import { auth } from "@/auth";
 
 export default async function Solves() {
-  const solves = await getAllSolves();
+  const session = await auth();
+  const userId = session?.user?.id;
+  const solves = await getSolvesByUser(userId);
+
+  if (!solves) {
+    return null;
+  }
 
   const solveList = solves.map((solve) => {
     const time = getTimeString(solve.time);
