356132b5dcfe9d06568fc202ddb38f54f421e464
Nick <nick.leblanc@unb.ca>
2024-02-19 20:01:16 -0400

    Added middleware to protect routes

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,41 @@
 import NextAuth from "next-auth";
 import { authConfig } from "@/auth.config";
+import { publicRoutes, authRoutes, apiAuthPrefix } from "@/routes";
 
-export default NextAuth(authConfig).auth;
+const { auth } = NextAuth(authConfig);
+
+export default auth((req) => {
+  const { nextUrl } = req;
+  const isLoggedIn = !!req.auth;
+
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  // const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+  if (isApiAuthRoute) {
+    return null;
+  }
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return Response.redirect("/");
+    }
+    return null;
+  }
+
+  if (!isLoggedIn) {
+    let callbackUrl = nextUrl.pathname;
+    if (nextUrl.search) {
+      callbackUrl += nextUrl.search;
+    }
+
+    const encodedCallbackUrl = encodeURIComponent(callbackUrl);
+    return Response.redirect(
+      new URL(`/login?callbackUrl=${encodedCallbackUrl}`, nextUrl),
+    );
+  }
+  return null;
+});
 
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
